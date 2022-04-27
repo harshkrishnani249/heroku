a515fb1c6f49789a0ebaffac0973e43ac6c7c1e5
Harsh Krishnani <harshkrishnani2409@gmail.com>
2022-04-28 01:16:34 +0530

    News

---
+++ src/routes/news.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const newsRouter = express.Router()
+const axios = require('axios')
+ 
+newsRouter.get('', async(req, res) => {
+    try {
+        // fetch data from MediaStack API
+        const newsAPI = await axios.get(`http://api.mediastack.com/v1/news?access_key=c26c934b723b31d9f8d830b2bf115385&countries=gb`)
+        // console.log(newsAPI.data)  // debugging
+ 
+        // transfer response to the news.ejs
+        res.render('news', { articles : newsAPI.data.data })  // first data is reponse next one is the articles in the response (array)
+    } catch (error) {
+        if(error.response) {
+            console.log(error.response.data)
+        } else if(error.request) {
+            console.log(error.request)
+        } else {
+            console.log('Error', error.message)
+        }
+    }
+})
+ 
+module.exports = newsRouter
